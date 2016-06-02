67c2e11ebdca0ff4974807319b2cda9da9071d45
nowaits <nowaits@live.cn>
2016-06-02 22:38:05 +0800

    [Bugfix] 1.光标位置保存/恢复 2.网页加载完显示光标

--- editor.js
+++ editor.js
@@ -12,96 +12,135 @@ function assert(condition, message) {
 }
 
 editor._format = function (cmd, val) {
-    document.execCommand(cmd, false, val || '');
+    window.focus()
+    return document.execCommand(cmd, false, val || '');
 }
 
 editor.Reformat = function () {
-    this._format('removeFormat')
+    return this._format('removeFormat')
 }
 
 editor.Bold = function () {
-    this._format('bold')
+    return this._format('bold')
 }
 
 editor.Italic = function () {
-    this._format('italic')
+    return this._format('italic')
 }
 
 editor.Underline = function () {
-    this._format('underline')
+    return this._format('underline')
 }
 
 editor.Strikethrough = function () {
-    this._format('strikethrough')
+    return this._format('strikethrough')
 }
 
 editor.Justifyleft = function () {
-    this._format('justifyleft')
+    return this._format('justifyleft')
 }
 
 editor.Justifycenter = function () {
-    this._format('justifycenter')
+    return this._format('justifycenter')
 }
 
 editor.Justifyright = function () {
-    this._format('justifyright')
+    return this._format('justifyright')
 }
 
 editor.Indent = function () {
-    this._format('indent')
+    return this._format('indent')
 }
 
 editor.Outdent = function () {
-    this._format('outdent')
+    return this._format('outdent')
 }
 
 editor.Forecolor = function (val) {
     assert(val, '数值不能为空')
-    this._format('forecolor', val)
+    return this._format('forecolor', val)
 }
 
 editor.Bgcolor = function (val) {
     assert(val, '数值不能为空')
-    this._format('hilitecolor', val)
+    return this._format('hilitecolor', val)
 }
 
 editor.Forecolor = function (val) {
     assert(val, '数值不能为空')
-    this._format('forecolor', val)
+    return this._format('forecolor', val)
 }
 
 editor.InsertLink = function (val) {
     assert(val, '数值不能为空')
-    this._format('createlink', val)
+    return this._format('createlink', val)
 }
 
 editor.InsertPicture = function (val) {
     assert(val, '数值不能为空')
-    this._format('insertimage', val)
+    return this._format('insertimage', val)
 }
 
 editor.Fontname = function (val) {
     assert(val, '数值不能为空')
-    this._format('fontname', val)
+    return this._format('fontname', val)
 }
 
 editor.Fontsize = function (val) {
     assert(val, '数值不能为空')
-    this._format('fontsize', val)
+    return this._format('fontsize', val)
 }
 
 editor.InertOrderList = function () {
-
-    this._format('insertorderedlist')
+    return this._format('insertorderedlist')
 }
 
 editor.InertUnorderList = function () {
-
-    this._format('insertunorderedlist')
+    return this._format('insertunorderedlist')
 }
 
-window.addEventListener('paste', function () {
-    if (typeof window.getSelection === "undefined") {
+editor.Content = function () {
+    return document.body.innerHTML
+}
+
+editor.SaveCaretPos = function () {
+    saveRangePosition()
+}
+
+editor.RestoreCaretPos = function () {
+    restoreRangePosition()
+}
+
+window.addEventListener('load', function (e) {
+    document.body.focus()
+})
+
+window.addEventListener('keydown', function (e) {
+});
+
+window.addEventListener('keyup', function (e) {
+
+    var keyCode = e.keyCode;
+
+    window.location.hash = `keyup-${keyCode}-${Math.random()}`
+
+    console.log(window.location.hash)
+
+    saveRangePosition()
+})
+
+window.addEventListener('focus', function (e) {
+    window.location.hash = `focus-${Math.random()}`
+})
+
+window.addEventListener('paste', function (e) {
+    
+    saveRangePosition()
+    
+    window.location.hash = `paste-${copiedData.type}-${Math.random()}`
+
+    if (!e.clipboardData || !e.clipboardData.items || e.clipboardData.items.length <= 0) {
+        console.log(['--->>', e.clipboardData.types, e.clipboardData.files])
         return;
     }
 
@@ -117,3 +156,30 @@ window.addEventListener('paste', function () {
 
     editor.InsertPicture(blobUrl)
 });
+
+
+function saveRangePosition() {
+    var range = window.getSelection().getRangeAt(0);
+    var sC = range.startContainer, eC = range.endContainer;
+
+    A = []; while (sC !== document.body) { A.push(getNodeIndex(sC)); sC = sC.parentNode }
+    B = []; while (eC !== document.body) { B.push(getNodeIndex(eC)); eC = eC.parentNode }
+
+    window.rp = { "sC": A, "sO": range.startOffset, "eC": B, "eO": range.endOffset };
+}
+
+function restoreRangePosition() {
+    document.body.focus()
+    var sel = window.getSelection(), range = sel.getRangeAt(0);
+    var x, C, sC = document.body, eC = document.body;
+
+    C = rp.sC; x = C.length; while (x--) sC = sC.childNodes[C[x]];
+    C = rp.eC; x = C.length; while (x--) eC = eC.childNodes[C[x]];
+
+    range.setStart(sC, rp.sO);
+    range.setEnd(eC, rp.eO);
+    sel.removeAllRanges();
+    sel.addRange(range)
+}
+
+function getNodeIndex(n) { var i = 0; while (n = n.previousSibling) i++; return i }
